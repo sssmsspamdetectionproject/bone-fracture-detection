d5de212202e32cf841af773faa778059042a74c8
SMS SPAM CLASSIFIER <143194928+sssmsspamdetectionproject@users.noreply.github.com>
2024-08-11 01:15:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if uploaded_image is not None:
     image_np = np.array(image)
     
     # Load the YOLO model
-    model_path = 'Bone_Fracture_Detection_YoloV8n_Model.pt'  # Update this path to your model
+    model_path = 'Bone_Fracture_Detection_YoloV8s_Model.pt'  # Update this path to your model
     model = load_model(model_path)
     
     if model is not None:
